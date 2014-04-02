17b2a30eef54f9ade2cb024cced568d6a459757a
Eric Crawford <e2crawfo@uwaterloo.ca>
2014-04-02 15:59:46 -0400

    Updated driver to cover recent changes.

--- driver.py
+++ driver.py
@@ -3,6 +3,12 @@ import numpy as np
 
 x = nengo_mpi.PythonMpiSimulatorChunk()
 y = np.array(np.arange(10.0))
+w = np.array(np.arange(10.0) + 1)
 z = np.random.random((5,5))
 x.add_signal(id(y), y)
 x.add_signal(id(z), z)
+x.add_signal(id(w), w)
+
+x.create_Reset(id(y), .1)
+x.create_Copy(id(w), id(y))
+x.create_Reset(id(w), id(y))
