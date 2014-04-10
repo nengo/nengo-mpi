9083213e324f3c9a2901815a54204d7a5ac325b7
Eric Crawford <e2crawfo@uwaterloo.ca>
2014-04-10 11:37:12 -0400

    Added an LIFRate test.

--- nengo_mpi/test/test_operators.py
+++ nengo_mpi/test/test_operators.py
@@ -71,6 +71,7 @@ def test_copy():
     t = sim.trange(dt=.001)
     plt.plot(t, sim.data[2][:, 0])
     plt.plot(t, sim.data[3][:, 0])
+    plt.legend(loc='best')
     plt.savefig('test_copy.pdf')
     plt.close()
 
@@ -107,61 +108,61 @@ def test_lif():
     sim = nengo_mpi.Simulator(model=None, init_mpi=init_mpi)
     sim.run(1.0)
 
-    t = sim.trange(dt=.001)
-    spikes = sim.data[1]
-    plt.plot(t, spikes[:, 0:10])
-    plt.savefig('test_lif.pdf')
-    plt.close()
 
     #rng = np.random.RandomState(85243)
 
-    #spikes = np.zeros((t_final / dt,) + J.shape)
-    J = J.reshape(-1, 1)
-
-    math_rates = nengo.LIF(D, tau_rc=tau_rc, tau_ref=tau_ref).rates(J, gain=np.ones(D), bias=np.zeros(D))
+    spikes = sim.data[1]
     sim_rates = spikes.sum(0)
+
+    J = J.reshape(-1, 1)
+    math_rates = nengo.LIF(D, tau_rc=tau_rc, tau_ref=tau_ref).rates(J, gain=np.ones(D), bias=np.zeros(D))
 
     plt.plot(J, sim_rates, label='sim')
     plt.plot(J, math_rates, label='math')
+    plt.legend(loc='best')
     plt.savefig('test_lif.pdf')
     plt.close()
     assert np.allclose(sim_rates, math_rates, atol=1, rtol=0.02)
 
+def test_lif_rate():
+    """Test that the dynamic model approximately matches the rates."""
+    D = 40
+    tau_rc = 0.02
+    tau_ref = 0.002
+    dt = 0.001
 
-#def test_lif_base(nl_nodirect):
-#    """Test that the dynamic model approximately matches the rates"""
-#    rng = np.random.RandomState(85243)
-#
-#    dt = 0.001
-#    n = 5000
-#    x = 0.5
-#    max_rates = rng.uniform(low=10, high=200, size=n)
-#    intercepts = rng.uniform(low=-1, high=1, size=n)
-#
-#    m = nengo.Network()
-#    with m:
-#        ins = nengo.Node(x)
-#        ens = nengo.Ensemble(
-#            nl_nodirect(n), 1, max_rates=max_rates, intercepts=intercepts)
-#        nengo.Connection(ins, ens.neurons, transform=np.ones((n, 1)))
-#        spike_probe = nengo.Probe(ens.neurons, "output")
-#
-#    sim = nengo.Simulator(m, dt=dt)
-#
-#    t_final = 1.0
-#    sim.run(t_final)
-#    spikes = sim.data[spike_probe].sum(0)
-#
-#    math_rates = ens.neurons.rates(
-#        x, *ens.neurons.gain_bias(max_rates, intercepts))
-#    sim_rates = spikes / t_final
-#    logger.debug("ME = %f", (sim_rates - math_rates).mean())
-#    logger.debug("RMSE = %f",
-#                 rms(sim_rates - math_rates) / (rms(math_rates) + 1e-20))
-#    assert np.sum(math_rates > 0) > 0.5 * n, (
-#        "At least 50% of neurons must fire")
-#    assert np.allclose(sim_rates, math_rates, atol=1, rtol=0.02)
+    J = np.arange(-2, 2, .1)
 
+    def make_random():
+        return J
+
+    def init_mpi(sim):
+
+        A = np.zeros(D)
+        B = np.zeros(D)
+
+        sim.add_signal(0, A)
+        sim.add_signal(1, B)
+
+        sim.mpi_sim.create_PyFunc(0, make_random, False)
+        sim.mpi_sim.create_SimLIFRate(D, tau_rc, tau_ref, dt, 0, 1)
+        sim.add_probe(1, 1, 1)
+
+    sim = nengo_mpi.Simulator(model=None, init_mpi=init_mpi)
+    sim.run(1.0)
+
+    output = sim.data[1]
+    sim_rates = output[-1, :] / dt
+
+    math_rates = nengo.LIF(D, tau_rc=tau_rc, tau_ref=tau_ref).rates(J, gain=np.ones(D), bias=np.zeros(D))
+
+    plt.plot(J, sim_rates, label='sim')
+    plt.plot(J, math_rates, label='math')
+    plt.legend(loc='best')
+    plt.savefig('test_lif_rate.pdf')
+    plt.close()
+
+    assert np.allclose(sim_rates, math_rates, atol=1, rtol=0.02)
 
 if __name__ == "__main__":
     nengo.log(debug=True)
