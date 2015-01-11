6f9e39112497354d0947195e741ae0cc1ddb71e2
Eric Crawford <e2crawfo@uwaterloo.ca>
2015-01-10 20:30:27 -0500

    Added submission_script.sh
    submission_script.sh. Used to submit jobs using benchmark.py to the
    scinet job queue.

---
+++ submission_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# MOAB/Torque submission script for SciNet GPC 
+#
+#PBS -l nodes=2:ppn=8,walltime=0:15:00
+#PBS -N nengo_mpi
+#PBS -m abe
+
+# load modules (must match modules used for compilation)
+module load intel/14.0.1
+module load python/2.7.5
+module load openmpi/intel/1.6.4
+module load cxxlibraries/boost/1.55.0-intel
+module load gcc/4.8.1
+module load use.own
+module load nengo
+
+# DIRECTORY TO RUN - $PBS_O_WORKDIR is directory job was submitted from
+cd /home/c/celiasmi/e2crawfo/nengo_mpi/scripts
+
+# EXECUTION COMMAND; -np = nodes*ppn
+mpirun -np 1 python benchmark.py > /scratch/c/celiasmi/e2crawfo/experiments/temp.txt
