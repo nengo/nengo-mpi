4203b34113ee6ec4d502380771ebe3be94912d00
Eric Crawford <e2crawfo@uwaterloo.ca>
2015-03-23 20:42:20 -0400

    Added some new partitioners.

--- nengo_mpi/partition.py
+++ nengo_mpi/partition.py
@@ -1,6 +1,11 @@
-from nengo import builder
-from nengo.connection import Connection
+from collections import defaultdict
 import warnings
+
+import networkx as nx
+import numpy as np
+from nengo.ensemble import Neurons
+
+from heapq import heapify, heappush, heappop
 
 import logging
 logger = logging.getLogger(__name__)
@@ -46,6 +51,382 @@ def top_level_partitioner(network, num_components, assignments=None):
         if network not in assignments:
             assignments[n] = component
             component = (component + 1) % num_components
+
+    return assignments
+
+
+class PriorityDict(dict):
+    """
+    Retrieved from: http://code.activestate.com/recipes/
+        522995-priority-dict-a-priority-queue-with-updatable-prio/
+
+    Dictionary that can be used as a priority queue.
+
+    Keys of the dictionary are items to be put into the queue, and values
+    are their respective priorities. All dictionary methods work as expected.
+    The advantage over a standard heapq-based priority queue is
+    that priorities of items can be efficiently updated (amortized O(1))
+    using code as 'thedict[item] = new_priority.'
+
+    The 'smallest' method can be used to return the object with lowest
+    priority, and 'pop_smallest' also removes it.
+
+    The 'sorted_iter' method provides a destructive sorted iterator.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(PriorityDict, self).__init__(*args, **kwargs)
+        self._rebuild_heap()
+
+    def _rebuild_heap(self):
+        self._heap = [(v, k) for k, v in self.iteritems()]
+        heapify(self._heap)
+
+    def smallest(self):
+        """Return the item with the lowest priority.
+
+        Raises IndexError if the object is empty.
+        """
+
+        heap = self._heap
+        v, k = heap[0]
+        while k not in self or self[k] != v:
+            heappop(heap)
+            v, k = heap[0]
+        return k
+
+    def pop_smallest(self):
+        """Return the item with the lowest priority and remove it.
+
+        Raises IndexError if the object is empty.
+        """
+
+        heap = self._heap
+        v, k = heappop(heap)
+        while k not in self or self[k] != v:
+            v, k = heappop(heap)
+        del self[k]
+        return k
+
+    def __setitem__(self, key, val):
+        # We are not going to remove the previous value from the heap,
+        # since this would have a cost O(n).
+
+        super(PriorityDict, self).__setitem__(key, val)
+
+        if len(self._heap) < 2 * len(self):
+            heappush(self._heap, (val, key))
+        else:
+            # When the heap grows larger than 2 * len(self), we rebuild it
+            # from scratch to avoid wasting too much memory.
+            self._rebuild_heap()
+
+    def setdefault(self, key, val):
+        if key not in self:
+            self[key] = val
+            return val
+        return self[key]
+
+    def update(self, *args, **kwargs):
+        # Reimplementing dict.update is tricky -- see e.g.
+        # http://mail.python.org/pipermail/python-ideas/2007-May/000744.html
+        # We just rebuild the heap from scratch after passing to super.
+        super(PriorityDict, self).update(*args, **kwargs)
+        self._rebuild_heap()
+
+    def sorted_iter(self):
+        """Sorted iterator of the priority dictionary items.
+
+        Beware: this will destroy elements as they are returned.
+        """
+        while self:
+            yield self.pop_smallest()
+
+
+def greedy_k(S, k, key=None):
+    if key is None:
+        key = lambda x: x
+
+    components = [set() for i in range(k)]
+    sizes = [0] * k
+
+    pq = PriorityDict()
+
+    pq.update({i: 0 for i in range(k)})
+
+    for n in sorted(S, reverse=True, key=key):
+
+        smallest = pq.pop_smallest()
+
+        components[smallest].add(n)
+        sizes[smallest] += key(n)
+        pq[smallest] = sizes[smallest]
+
+    return components, sizes
+
+
+def work_balanced_partitioner(network, num_components, assignments=None):
+    """
+    Tries to give each processor an equal number of neurons to simulate,
+    and makes no attempt to minimize communication between processors.
+
+    Parameters
+    ----------
+    network: The nengo network to partition.
+
+    num_components: The number of components to divide the nengo graph into.
+        If None, appropriate value chosen automatically.
+
+    assignments: A dictionary mapping from nengo objects to component indices,
+        with 0 as the first component. Used to hard-assign nengo objects
+        to specific components.
+    """
+
+    if assignments is not None:
+        assignments = assignments.copy()
+    else:
+        assignments = {}
+
+    if num_components == 1:
+        return {}
+
+    components, sizes = greedy_k(
+        network.all_ensembles, num_components, key=lambda e: e.n_neurons)
+
+    for i, c in enumerate(components):
+        for e in c:
+            assignments[e] = i
+
+    return assignments
+
+
+class GraphNode(object):
+    def __init__(self):
+        self.objects = set()
+        self.inputs = set()
+        self.outputs = set()
+
+    def add_object(self, obj):
+        self.objects.add(obj)
+
+    def add_input(self, i):
+        self.inputs.add(i)
+
+    def add_output(self, o):
+        self.outputs.add(o)
+
+    def empty(self):
+        return len(self.objects) == 0
+
+    def __str__(self):
+        s = ",".join(str(o) for o in self.objects)
+        return s
+
+
+def is_update(conn):
+    return conn.synapse is not None
+
+
+def network_to_filter_graph(network):
+    """
+    Creates a graph from a nengo network, where the nodes are collections
+    of nengo objects that are connect by non-filtered connections, and edges
+    are filtered connections between those components. This is useful for
+    partitioning, we can only send data across connections that contain an
+    update operation, which, for now, means they must be filtered.
+
+    Parameters
+    ----------
+    network: The nengo network to partition.
+    """
+
+    def merge_nodes(node_map, a, b):
+        a.objects = a.objects.union(b.objects)
+        a.inputs = a.inputs.union(b.inputs)
+        a.outputs = a.outputs.union(b.outputs)
+
+        for obj in b.objects:
+            node_map[obj] = a
+
+        del b
+
+    node_map = defaultdict(GraphNode)
+
+    for conn in network.all_connections:
+
+        pre_obj = conn.pre_obj
+
+        if isinstance(pre_obj, Neurons):
+            pre_obj = pre_obj.ensemble
+
+        pre_node = node_map[pre_obj]
+
+        if pre_node.empty():
+            pre_node.add_object(pre_obj)
+
+        post_obj = conn.post_obj
+
+        if isinstance(post_obj, Neurons):
+            post_obj = post_obj.ensemble
+
+        post_node = node_map[post_obj]
+
+        if post_node.empty():
+            post_node.add_object(post_obj)
+
+
+        if is_update(conn):
+            pre_node.add_output(conn)
+            post_node.add_input(conn)
+        else:
+            merge_nodes(node_map, pre_node, post_node)
+
+    G = nx.Graph()
+
+    update_connections = filter(
+        is_update, network.all_connections)
+
+    f = lambda e: e.ensemble if isinstance(e, Neurons) else e
+
+    G.add_weighted_edges_from(
+        (node_map[f(conn.pre_obj)], node_map[f(conn.post_obj)], conn.size_mid)
+        for conn in update_connections)
+
+    return G
+
+
+def stoer_wagner_partitioner(network, num_components, assignments=None):
+    """
+
+    Parameters
+    ----------
+    network: The nengo network to partition.
+
+    num_components: The number of components to divide the nengo graph into.
+        If None, appropriate value chosen automatically.
+
+    assignments: A dictionary mapping from nengo objects to component indices,
+        with 0 as the first component. Used to hard-assign nengo objects
+        to specific components.
+    """
+
+    if assignments is not None:
+        assignments = assignments.copy()
+    else:
+        assignments = {}
+
+    if num_components == 1:
+        return {}
+
+    G = network_to_filter_graph(network)
+
+    sizes = {'': len(G)}
+    components = {'': G}
+    names = ['']
+
+    while len(components) < num_components:
+        max_name = max(names, key=sizes.__getitem__)
+
+        G = components[max_name]
+
+        # requires networkx 1.9
+        cut_value, partition = nx.stoer_wagner(G)
+
+        names.remove(max_name)
+
+        del sizes[max_name]
+        del components[max_name]
+
+        A_name = max_name + "A"
+        B_name = max_name + "B"
+
+        names.extend([A_name, B_name])
+
+        components[A_name] = G.subgraph(partition[0])
+        sizes[A_name] = len(partition[0])
+
+        components[B_name] = G.subgraph(partition[1])
+        sizes[B_name] = len(partition[1])
+
+    for i, g in enumerate(components.values()):
+        for node in g.nodes():
+            for obj in node.objects:
+                assignments[obj] = i
+
+    return assignments
+
+
+def count_neurons(network):
+    def helper(network, counts):
+        n_neurons = 0
+
+        for e in network.ensembles:
+            n_neurons += e.n_neurons
+
+        counts[network, 0] = n_neurons
+
+        for n in network.networks:
+            n_neurons += helper(n, counts)
+
+        counts[network] = n_neurons
+
+        return n_neurons
+
+    counts = {}
+    helper(network, counts)
+
+    return counts
+
+
+def spectral_partitioner(network, num_components, assignments=None):
+    """
+
+    Parameters
+    ----------
+    network: The nengo network to partition.
+
+    num_components: The number of components to divide the nengo graph into.
+        If None, appropriate value chosen automatically.
+
+    assignments: A dictionary mapping from nengo objects to component indices,
+        with 0 as the first component. Used to hard-assign nengo objects
+        to specific components.
+    """
+
+    if assignments is not None:
+        assignments = assignments.copy()
+    else:
+        assignments = {}
+
+    if num_components == 1:
+        return {}
+
+    G = network_to_filter_graph(network)
+
+    neuron_counts = count_neurons(network)
+
+    ordering = nx.spectral_ordering(G)
+
+    print "Max node size in filter graph: ", max(len(n.objects) for n in G.nodes())
+    print (
+        "Max node size in filter graph: ",
+        max((n.objects for n in G.nodes()), key=lambda x: len(x)))
+
+    neurons_per_component = float(neuron_counts[network]) / num_components
+
+    component = 0
+    n_neurons = 0
+
+    for gnode in ordering:
+        for obj in gnode.objects:
+            assignments[obj] = component
+
+            if hasattr(obj, 'n_neurons'):
+                n_neurons += obj.n_neurons
+
+        if n_neurons > neurons_per_component:
+            component += 1
+            n_neurons = 0
 
     return assignments
 
@@ -229,3 +610,64 @@ def propogate_assignments(network, assignments):
     probes = network.all_probes
     probes_in = all([probe in assignments for probe in probes])
     assert probes_in, "Assignments incomplete, missing probes."
+
+
+def evaluate_partition(network, num_components, assignments):
+    component_neuron_counts = [0] * num_components
+    component_item_counts = [0] * num_components
+
+    for ens in network.all_ensembles:
+        if ens in assignments:
+            component_neuron_counts[assignments[ens]] += ens.n_neurons
+            component_item_counts[assignments[ens]] += 1
+
+    for node in network.all_nodes:
+        if node in assignments:
+            component_item_counts[assignments[ens]] += 1
+
+    mean_neuron_count = np.mean(component_neuron_counts)
+    neuron_count_std = np.std(component_neuron_counts)
+
+    print "*" * 80
+    print "Evaluating partition of network"
+
+    print "Total number of neurons: ", count_neurons(network)[network]
+    print "Mean neurons per cluster: ", mean_neuron_count
+    print "Standard deviation of neurons per cluster", neuron_count_std
+    print "Min number of neurons", np.min(component_neuron_counts)
+    print "Max number of neurons", np.max(component_neuron_counts)
+    print (
+        "Number of empty partitions",
+        num_components - np.count_nonzero(component_neuron_counts))
+
+    mean_item_count = np.mean(component_item_counts)
+    item_count_std = np.std(component_item_counts)
+
+    print "*" * 10
+
+    print "Evaluating partition of network"
+    print "Total number of items: ", len(network.all_nodes + network.all_ensembles)
+    print "Mean items per cluster: ", mean_item_count
+    print "Standard deviation of items per cluster", item_count_std
+    print "Min number of items", np.min(component_item_counts)
+    print "Max number of items", np.max(component_item_counts)
+    print (
+        "Number of empty partitions",
+        num_components - np.count_nonzero(component_item_counts))
+
+    communication_weight = 0
+    total_weight = 0
+
+    for conn in network.all_connections:
+        if is_update(conn):
+            if assignments[conn.pre_obj] != assignments[conn.post_obj]:
+                communication_weight += conn.size_mid
+
+            total_weight += conn.size_mid
+
+    print "*" * 10
+    print "Number of dimensions that are communicated: ", communication_weight
+    print "Total number of filtered dimensions: ", total_weight
+    print (
+        "Percentage of filtered dimensions that are "
+        "communicated: ", float(communication_weight) / total_weight)
