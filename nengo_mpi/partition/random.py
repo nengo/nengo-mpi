776bf6f8db997bec41ec41e0ebc6e7f7353bc657
Eric Crawford <e2crawfo@uwaterloo.ca>
2015-05-16 12:47:40 -0400

    Added random partitioner (useful as a baseline).

---
+++ nengo_mpi/partition/random.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def random_partitioner(filter_graph, n_components, seed=None):
+    """
+    Assigns each filter_graph node to a random component.
+
+    Parameters
+    ----------
+    filter_graph: networkx Graph
+        A graph created from a network using network_to_filter_graph.
+
+    n_components: int
+        Desired number of components in the partition.
+
+    seed: int
+        Seed for the random number generator.
+
+    Returns
+    -------
+    assignments: dict
+        A mapping from nodes in the filter graph to components.
+    """
+    assert n_components > 1
+
+    rng = np.random.RandomState(seed)
+    random_ints = rng.random_integers(0, n_components-1, size=len(filter_graph))
+    assignments = {n: c for n, c in zip(filter_graph, random_ints)}
+
+    return assignments
