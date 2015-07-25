c7f677a4353033d4700e2da57f39d89ed2d99075
Eric Crawford <e2crawfo@uwaterloo.ca>
2015-07-25 16:07:27 -0400

    Fixed testing bug.
    When importing tests from nengo, was getting a KeyError: 'compare'. In
    nengo/tests/conftest.py we can see multiple checks for a 'compare' key.
    This key is created by the code in nengo/tests/options.py, but that code
    is apparently not executed when importing tests. So I just copy-pasted
    into nengo_mpi's conftest.py. One other small change had to be made, as
    the 'compare' key was still not present. Had to add a default=None to
    the compare argument in the code stolen from options.py.

--- nengo_mpi/tests/conftest.py
+++ nengo_mpi/tests/conftest.py
@@ -4,6 +4,26 @@ import nengo
 import nengo_mpi
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        '--plots', nargs='?', default=False, const=True,
+        help='Save plots (can optionally specify a directory for plots).')
+    parser.addoption(
+        '--analytics', nargs='?', default=False, const=True,
+        help='Save analytics (can optionally specify a directory for data).')
+    parser.addoption(
+        '--compare', nargs=2, default=None,
+        help='Compare analytics results (specify directories to compare).')
+    parser.addoption(
+        '--logs', nargs='?', default=False, const=True,
+        help='Save logs (can optionally specify a directory for logs).')
+    parser.addoption('--noexamples', action='store_false', default=True,
+                     help='Do not run examples')
+    parser.addoption(
+        '--slow', action='store_true', default=False,
+        help='Also run slow tests.')
+
+
 def Mpi2Simulator(*args, **kwargs):
     return nengo_mpi.Simulator(*args, **kwargs)
 
