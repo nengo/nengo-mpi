10bd4e6ebb28612fb0416ecb92c105ae6ebb4a56
Eric Crawford <eric.crawford@mail.mcgill.ca>
2016-01-19 10:41:49 -0500

    Removed nengo as dep for docs.

--- docs/conf.py
+++ docs/conf.py
@@ -6,11 +6,10 @@
 import sys
 
 try:
-    import nengo
     import nengo_mpi
     import sphinx_rtd_theme
 except ImportError:
-    print ("To build the documentation, nengo, nengo_mpi and sphinx_rtd_theme "
+    print ("To build the documentation, nengo_mpi and the sphinx_rtd_theme "
            "must be installed in the current environment. Please install these "
            "and their requirements first. A virtualenv is recommended!")
     sys.exit(1)
@@ -22,7 +21,6 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
     'numpydoc',
-    'nengo.utils.docutils',
 ]
 
 # -- sphinx.ext.todo config
